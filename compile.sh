b4bfc75852953ea8322dda4c4d0e4a76808a2a60
merc1er <corentin@mercier.link>
2025-03-15 00:25:19 +0700

    Execute commands

---
+++ compile.sh
@@ -0,0 +1,1 @@
+g++ -std=c++23 src/main.cpp -o main
--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,13 @@
 #include <vector>
 #include <string>
 #include <ranges>
+#include <cstdlib>
 #include <filesystem>
 
 std::string BUILTINS[] = {"echo", "exit", "type"};
 const char *PATH = std::getenv("PATH");
 
-std::vector<std::string> split(std::string_view path, char delim = ':')
+std::vector<std::string> split(std::string_view path, char delim)
 {
   std::vector<std::string> paths;
   for (auto &&part : std::views::split(path, delim))
@@ -15,6 +16,19 @@ std::vector<std::string> split(std::string_view path, char delim = ':')
     paths.emplace_back(part.begin(), part.end());
   }
   return paths;
+}
+
+std::string get_command_path(std::string &command)
+{
+  for (std::string path : split(PATH, ':'))
+  {
+    const std::string full_path = path + "/" + command;
+    if (std::filesystem::exists(full_path))
+    {
+      return full_path;
+    }
+  }
+  return "";
 }
 
 void execute_type_command(std::string &input)
@@ -29,30 +43,18 @@ void execute_type_command(std::string &input)
     {
       std::cout << command << " is a shell builtin" << std::endl;
       found = true;
+      return;
     }
   }
 
-  // Check if the command is in the PATH.
-  if (!found)
+  const std::string full_path = get_command_path(command);
+  if (full_path != "")
   {
-    for (std::string path : split(PATH))
-    {
-      const std::string full_path = path + "/" + command;
-      if (std::filesystem::exists(full_path))
-      {
-        std::cout << command << " is " << full_path << std::endl;
-        found = true;
-        break;
-      }
-    }
-  }
-
-  if (found)
-  {
+    std::cout << command << " is " << full_path << std::endl;
     return;
   }
+
   std::cout << command << ": not found" << std::endl;
-  return;
 }
 
 void run_shell()
@@ -62,22 +64,46 @@ void run_shell()
   std::string input;
   std::getline(std::cin, input);
 
+  // Exit builtin.
   if (input.starts_with("exit"))
   {
     std::exit(0);
   }
 
+  // Echo builtin.
   if (input.starts_with("echo"))
   {
     std::cout << input.substr(5) << std::endl;
     return;
   }
 
+  // Type builtin.
   if (input.starts_with("type"))
   {
     return execute_type_command(input);
   }
 
+  // Execute the command, if found in path.
+  std::vector<std::string> parts = split(input, ' ');
+  std::string command = parts[0];
+  std::string args = "";
+  if (parts.size() > 1)
+  {
+    args = input.substr(command.size() + 1);
+  }
+
+  if (command == "")
+  {
+    return;
+  }
+  std::string command_path = get_command_path(command);
+  if (command_path != "")
+  {
+    std::system((command + " " + args).c_str());
+    return;
+  }
+
+  // Command not found.
   std::cout << input << ": command not found" << std::endl;
 }
 
